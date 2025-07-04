f3748559fecd622e62b9445925a11361db051897
Your Name <you@example.com>
2025-07-04 10:57:28 +0530

    modern ways

--- Arrays/Countevenodd.js
+++ Arrays/Countevenodd.js
@@ -46,3 +46,42 @@ function countEvenOdd(arr){
 }
 
 console.log(countEvenOdd([1,2,3,4,5,6,7,8,9]))
+
+// Modern Loop Techniques
+// 🔹 Using for...of
+
+function  countEvenOdd(arr){
+      let even = 0;
+  let odd = 0;
+
+for(let num of arr){
+    if (num % 2 === 0) {
+      even++;
+    } else {
+      odd++;
+    }
+  }
+
+  return { even, odd };
+
+}
+
+// 🔹 Using forEach()
+// forEach() is a high-order function, great for short, readable loops.
+
+
+function countEvenOdd(arr){
+      let even = 0;
+  let odd = 0;
+
+  arr.forEach((num)=>{
+    if (num % 2 === 0) {
+      even++;
+    } else {
+      odd++;
+    }
+  });
+
+  return { even, odd };
+  
+}
