6ee709543b8b02e0359f98ae1866be5bc0f6b001
Your Name <you@example.com>
2025-07-05 10:54:14 +0530

    removeduplicate

---
+++ Arrays/removeduplicate.js
@@ -0,0 +1,19 @@
+// problem 7:  Remove Duplicates from a Sorted Array
+
+
+
+
+function removeDuplicates(arr){
+    if(arr.length===0)return [];
+
+    let result=[arr[0]];
+
+    for(let i=1;i<arr.length;i++){
+        if(arr[i]!==arr[i-1]){
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
+
+console.log(removeDuplicates([1, 1, 2, 3, 3]));
