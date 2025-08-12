697e8fc8bcb0cc06941ca1c33f3cb6c36de98e93
Your Name <gagansalian04102005@gmail.com>
2025-08-12 10:15:27 +0530

    peak

--- BinarySearchAlgo/findthepeak.js
+++ BinarySearchAlgo/findthepeak.js
@@ -6,3 +6,10 @@
 // Then it strictly decreases.
 
 // Goal: Find the index of the peak element in O(log n) time.
+
+// Example:
+// js
+// Copy
+// Edit
+// Input: [0, 2, 4, 5, 3, 1]
+// Output: 3  // (value 5 is the peak)
