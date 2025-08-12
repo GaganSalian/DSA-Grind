add156b63de08800cf8f0c00154774ae0fe4a49d
Your Name <gagansalian04102005@gmail.com>
2025-08-12 10:13:58 +0530

    peak

---
+++ BinarySearchAlgo/findthepeak.js
@@ -0,0 +1,8 @@
+// 🧩 Problem 78
+// You’re given a mountain array — meaning:
+
+// It strictly increases until some peak element.
+
+// Then it strictly decreases.
+
+// Goal: Find the index of the peak element in O(log n) time.
